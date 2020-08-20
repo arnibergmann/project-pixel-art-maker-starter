fc358ce09833c2410856410f544eca337c5f94d3
arnibergmann <69400454+arnibergmann@users.noreply.github.com>
2020-08-20 12:22:19 +0200

    I added descriptive comments to my js file

--- designs.js
+++ designs.js
@@ -1,5 +1,3 @@
-// Select color input
-// Select size input
 const colorPicker = document.getElementById('colorPicker');
 const rowsNumber = document.getElementById('inputHeight');
 const cellsNumber = document.getElementById('inputWidth');
@@ -7,19 +5,21 @@ const pixelCanvas = document.getElementById('pixelCanvas');
 const form = document.getElementById('sizePicker');
 
 
-// When size is submitted by the user, call makeGrid()
+// This eventListener changes the size of the grid
 form.addEventListener('submit', function (e) {
     pixelCanvas.innerHTML = '';
     e.preventDefault();
     makeGrid();
 });
 
+// This eventListener allows the user to choose color
 pixelCanvas.addEventListener('click', function (e) {
     if (e.target.nodeName === 'TD') {
         e.target.style.backgroundColor = colorPicker.value;
     }
 })
 
+// This function makes the grid for the user
 function makeGrid() {
     for (let i = 0; i < rowsNumber.value; i++) {
         const row = pixelCanvas.insertRow(0);
@@ -27,7 +27,4 @@ function makeGrid() {
             row.insertCell(0);
         }
     }
-
-    // Your code goes here!
-
 }
